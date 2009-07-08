cd9333ced902975946539af5a5a963f09a09861f
W. Andrew Loe III <andrew@andrewloe.com>
2009-07-08 16:19:55 -0700

    Hooking up to AWS.

--- lib/pruner.rb
+++ lib/pruner.rb
@@ -1,3 +1,17 @@
+require 'rubygems'
+require 'yaml'
+require 'right_aws'
+
+require 'pruner/version'
+
 class Pruner
-  VERSION = '1.0.0'
+  attr_reader :config, :ec2
+  
+  def initialize(config_file_path)
+    @config = YAML.load_file(File.dirname(__FILE__) + "/#{config_file_path}").symbolize_keys
+  end
+  
+  def ec2
+    @ec2 ||= RightAws::Ec2.new(config[:access_key_id], config[:secret_access_key])
+  end
 end
--- test/test_pruner.rb
+++ test/test_pruner.rb
@@ -1,8 +1,30 @@
-require "test/unit"
-require "pruner"
+require 'test/unit'
+require 'fakeweb'
+require 'pruner'
 
 class TestPruner < Test::Unit::TestCase
-  def test_sanity
-    flunk "write tests or I will kneecap you"
+  
+  def new_pruner
+    @pruner = Pruner.new('../config.yml')
+  end
+  
+  def stub_aws
+    FakeWeb.register_uri(:any, 'https://ec2.amazonaws.com:443', :status => ['200', 'OK'])
+  end
+  
+  def test_config_parsing
+    new_pruner
+    assert_equal @pruner.config[:access_key_id], 'access_key'
+    assert_equal @pruner.config[:secret_access_key], 'secret_access_key'
+  end
+  
+  def test_ec2
+    stub_aws
+    new_pruner
+    assert_not_nil @pruner.ec2
+  end
+  
+  def test_version
+    assert_not_nil Pruner::VERSION
   end
 end
