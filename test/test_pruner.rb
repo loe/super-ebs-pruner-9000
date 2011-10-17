7164d1953ec70f5e5fcf372b77c46d1f2a1fac46
W. Andrew Loe III <andrew@andrewloe.com>
2011-10-17 16:43:03 -0700

    Tests supporting new rules.

--- test/test_pruner.rb
+++ test/test_pruner.rb
@@ -1,5 +1,4 @@
 require 'test/unit'
-require 'rubygems'
 require 'mocha'
 require 'pruner'
 
@@ -140,46 +139,39 @@ class TestPruner < Test::Unit::TestCase
     @pruner.volumes = ['vol-foo']
     @mock_ec2.expects(:describe_snapshots).returns(twice_hourly_yesterday('vol-foo'))
     @pruner.apply_rule(Pruner::HOURLY_AFTER_A_DAY)
-    assert_equal @pruner.old_snapshots.size, 24
+    assert_equal 24, @pruner.old_snapshots.size
   end
   
   def test_apply_rule_DAILY_AFTER_A_WEEK
     @pruner.volumes = ['vol-foo']
     @mock_ec2.expects(:describe_snapshots).returns(twice_daily_last_week('vol-foo'))
     @pruner.apply_rule(Pruner::DAILY_AFTER_A_WEEK)
-    assert_equal @pruner.old_snapshots.size, 7
+    assert_equal 7, @pruner.old_snapshots.size
   end
   
-  def test_apply_rule_EVERY_OTHER_DAY_AFTER_A_MONTH
+  def test_apply_rule_WEEKLY_AFTER_A_MONTH
     @pruner.volumes = ['vol-foo']
     @mock_ec2.expects(:describe_snapshots).returns(daily_last_month('vol-foo'))
-    @pruner.apply_rule(Pruner::EVERY_OTHER_DAY_AFTER_A_MONTH)
-    assert_equal @pruner.old_snapshots.size, 15
+    @pruner.apply_rule(Pruner::WEEKLY_AFTER_A_MONTH)
+    assert_equal 25, @pruner.old_snapshots.size
   end
   
-  def test_apply_rule_WEEKLY_AFTER_A_QUARTER
+  def test_apply_rule_MONTHLY_AFTER_A_QUARTER
     @pruner.volumes = ['vol-foo']
     @mock_ec2.expects(:describe_snapshots).returns(twice_weekly_last_quarter('vol-foo'))
-    @pruner.apply_rule(Pruner::WEEKLY_AFTER_A_QUARTER)
-    assert_equal @pruner.old_snapshots.size, 6
-  end
-  
-  def test_apply_rule_EVERY_THREE_WEEKS_AFTER_TWO_YEARS
-    @pruner.volumes = ['vol-foo']
-    @mock_ec2.expects(:describe_snapshots).returns(weekly_two_years_ago('vol-foo'))
-    @pruner.apply_rule(Pruner::EVERY_THREE_WEEKS_AFTER_TWO_YEARS)
-    assert_equal @pruner.old_snapshots.size, 32
+    @pruner.apply_rule(Pruner::MONTHLY_AFTER_A_QUARTER)
+    assert_equal 10, @pruner.old_snapshots.size
   end
   
   def test_apply_rules
     @pruner.volumes = ['vol-foo']
     @mock_ec2.expects(:describe_snapshots).returns(snapshots_array_foo)
     @pruner.apply_rules
-    assert_equal @pruner.old_snapshots.size, 24 + 7 + 15 + 6 + 32
+    assert_equal 24 + 7 + 25 + 10 + 49, @pruner.old_snapshots.size
   end
   
   def test_remove_snapshots
-    dead_snaps = 24 + 7 + 15 + 6 + 32
+    dead_snaps = 24 + 7 + 25 + 10 + 49
     @pruner.volumes = ['vol-foo']
     @mock_ec2.expects(:describe_snapshots).returns(snapshots_array_foo)
     @mock_ec2.expects(:delete_snapshot).times(dead_snaps)
@@ -188,7 +180,7 @@ class TestPruner < Test::Unit::TestCase
   end
   
   def test_one_volume_prune!
-    dead_snaps = 24 + 7 + 15 + 6 + 32
+    dead_snaps = 24 + 7 + 25 + 10 + 49
     @mock_ec2.expects(:describe_volumes).returns([{:aws_id => "vol-foo"}]).once
     @mock_ec2.expects(:describe_snapshots).returns(snapshots_array_foo).once
     @mock_ec2.expects(:delete_snapshot).times(dead_snaps)
@@ -196,7 +188,7 @@ class TestPruner < Test::Unit::TestCase
   end
   
   def test_multiple_volumes_prune!
-    dead_snaps = (24 + 7 + 15 + 6 + 32) * 3 # one for each array!
+    dead_snaps = (24 + 7 + 25 + 10 + 49) * 3 # one for each array!
     @mock_ec2.expects(:describe_volumes).returns(volumes_array).once
     @mock_ec2.expects(:describe_snapshots).returns(snapshots_array_foo + snapshots_array_bar + snapshots_array_baz).once
     @mock_ec2.expects(:delete_snapshot).times(dead_snaps)
